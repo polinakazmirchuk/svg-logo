63bc7f1d8b7e47c10214946cf2d319a7a0c37474
Polina Kazmirchuk <kazmircukpolina1mp@gmail.com>
2023-01-13 22:48:01 +0200

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("button").style.backgroundColor = "#0C192B";
+}
+
+element.addEventListener("click", Function);
+
+function Function() {
+  document.getElementById("button").style.backgroundColor = "#ffffff";
+}
